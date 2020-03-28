1969ef4731a2055a668ecf7d5e0fe9fbfe88bcfc
Niroigen Suntharam <niroigen@gmail.com>
2020-03-28 18:26:12 -0400

    Simulating time and getting the end time of each robotdata#.log file

--- driver.m
+++ driver.m
@@ -1,1 +1,24 @@
-extract_data('robotdata1.log');
+LASER_TIME_IDX = 187;
+ODOMETRY_TIME_IDX = 4;
+DELTA_T = 0.01;
+
+[laser, odometry, end_time] = extract_data('robotdata1.log');
+
+curr_laser_data = 1;
+curr_odometry_data = 1;
+
+for t = 0:DELTA_T:end_time
+    disp(t)
+
+    if laser(curr_laser_data,LASER_TIME_IDX) >= t - DELTA_T && laser(curr_laser_data,LASER_TIME_IDX) <= t
+        %% PERFORM SENSOR MODEL UPDATE
+        curr_laser_data += 1;
+    end
+
+    if odometry(curr_odometry_data,ODOMETRY_TIME_IDX) >= t - DELTA_T && odometry(curr_odometry_data,ODOMETRY_TIME_IDX) <= t
+        %% PERFORM MOTION MODEL UPDATE
+        curr_odometry_data += 1;
+    end
+
+    %% UPDATE BELIEF
+end
